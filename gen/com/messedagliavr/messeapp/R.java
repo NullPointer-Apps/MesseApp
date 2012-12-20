7f328bd28b9ae07dfb54e10387ae30ee26146bfc
Simone de Blasiis <simone.deblasiis@gmail.com>
2012-12-20 15:50:24 +0100

    NON FUNZIONANTE: Prova per introdurre il parser
    Genera un'eccezione strana: dice che devo dichiarare una List che con
    nome R.android.list che non viene mai richiamata dal codice.

--- gen/com/messedagliavr/messeapp/R.java
+++ gen/com/messedagliavr/messeapp/R.java
@@ -18,21 +18,25 @@ public final class R {
     }
     public static final class id {
         public static final int imageView1=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int list=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int product_label=0x7f070003;
         public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_item=0x7f030001;
+        public static final int single_item=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int Circolari=0x7f040004;
+        public static final int Circolari=0x7f040003;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int noconnection=0x7f040004;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/com/messedagliavr/messeapp/MainActivity.java
+++ src/com/messedagliavr/messeapp/MainActivity.java
@@ -1,10 +1,32 @@
 package com.messedagliavr.messeapp;
 
+
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	public boolean CheckInternet() {
+		ConnectivityManager connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		android.net.NetworkInfo wifi = connec
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		android.net.NetworkInfo mobile = connec
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+		if (wifi.isConnected()) {
+			return true;
+		} else if (mobile.isConnected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,4 +39,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    public void news(View view) {
+		if (CheckInternet() == true) {
+			startActivity(new Intent(this, news.class));
+		} else {
+			Toast.makeText(MainActivity.this, R.string.noconnection,
+					Toast.LENGTH_LONG).show();
+		}
+	}
 }
