7c3bf62892ecdde05ba97c65c79a8c111bd2b3b3
Francesco <ambro.f@hotmail.com>
2013-02-17 14:23:43 +0100

    SUPER LAYOUT COMMIT

--- gen/com/messedagliavr/messeapp/R.java
+++ gen/com/messedagliavr/messeapp/R.java
@@ -10,6 +10,9 @@ package com.messedagliavr.messeapp;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int ldpi_320x240=0x7f040000;
+    }
     public static final class drawable {
         public static final int calendario=0x7f020000;
         public static final int circolari=0x7f020001;
@@ -25,6 +28,7 @@ public final class R {
         public static final int youtube=0x7f02000b;
     }
     public static final class id {
+<<<<<<< HEAD
         public static final int DescrizioneView=0x7f070018;
         public static final int ImageCalendario=0x7f07000b;
         public static final int ImageCircolari=0x7f07000a;
@@ -56,6 +60,40 @@ public final class R {
         public static final int product_label=0x7f070019;
         public static final int social=0x7f07001a;
         public static final int textAmbro=0x7f07000e;
+=======
+        public static final int DescrizioneView=0x7f080019;
+        public static final int ImageCalendario=0x7f080007;
+        public static final int ImageCircolari=0x7f08000b;
+        public static final int ImageFoto=0x7f080003;
+        public static final int ImageNotizie=0x7f080005;
+        public static final int ImageSocial=0x7f080001;
+        public static final int ImageView01=0x7f08001c;
+        public static final int ImageView02=0x7f08001d;
+        public static final int ImageVoti=0x7f080009;
+        public static final int ScrollView01=0x7f080017;
+        public static final int TextAmbro=0x7f08000e;
+        public static final int TextBenati=0x7f080013;
+        public static final int TextBianchi=0x7f080015;
+        public static final int TextCalendario=0x7f080008;
+        public static final int TextCircolari=0x7f08000c;
+        public static final int TextDebla=0x7f08000f;
+        public static final int TextDev=0x7f08000d;
+        public static final int TextFoto=0x7f080004;
+        public static final int TextGraphic=0x7f080010;
+        public static final int TextGrezz=0x7f080011;
+        public static final int TextIervasi=0x7f080014;
+        public static final int TextNotizie=0x7f080006;
+        public static final int TextOtdev=0x7f080012;
+        public static final int TextSocial=0x7f080002;
+        public static final int TextVoti=0x7f08000a;
+        public static final int TitoloView=0x7f080018;
+        public static final int activity_main=0x7f080000;
+        public static final int exit=0x7f08001f;
+        public static final int info=0x7f08001e;
+        public static final int product_label=0x7f08001a;
+        public static final int social=0x7f08001b;
+        public static final int textAmbro=0x7f080016;
+>>>>>>> SUPER LAYOUT COMMIT
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -66,37 +104,37 @@ public final class R {
         public static final int social=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
-        public static final int list_item_selected=0x7f060001;
+        public static final int activity_main=0x7f070000;
+        public static final int list_item_selected=0x7f070001;
     }
     public static final class string {
-        public static final int Calendario=0x7f040003;
-        public static final int Circolari=0x7f040008;
-        public static final int Foto=0x7f040007;
-        public static final int Info=0x7f040001;
-        public static final int Notizie=0x7f040006;
-        public static final int Social=0x7f040004;
-        public static final int Voti=0x7f040005;
-        public static final int ambro=0x7f04000b;
-        public static final int app_name=0x7f040000;
-        public static final int benati=0x7f04000e;
-        public static final int bianchi=0x7f04000f;
-        public static final int canceledcalendar=0x7f040018;
-        public static final int cancelednews=0x7f040017;
-        public static final int debla=0x7f04000c;
-        public static final int dev=0x7f040011;
-        public static final int exit=0x7f040009;
-        public static final int graphic=0x7f040012;
-        public static final int grezzana=0x7f04000d;
-        public static final int iervasi=0x7f040010;
-        public static final int main_title=0x7f040002;
-        public static final int noconnection=0x7f04000a;
-        public static final int noconnectioncalendar=0x7f040014;
-        public static final int nodescription=0x7f040015;
-        public static final int notavailable=0x7f040016;
-        public static final int otdev=0x7f040013;
+        public static final int Calendario=0x7f050003;
+        public static final int Circolari=0x7f050008;
+        public static final int Foto=0x7f050007;
+        public static final int Info=0x7f050001;
+        public static final int Notizie=0x7f050006;
+        public static final int Social=0x7f050004;
+        public static final int Voti=0x7f050005;
+        public static final int ambro=0x7f05000b;
+        public static final int app_name=0x7f050000;
+        public static final int benati=0x7f05000e;
+        public static final int bianchi=0x7f05000f;
+        public static final int canceledcalendar=0x7f050018;
+        public static final int cancelednews=0x7f050017;
+        public static final int debla=0x7f05000c;
+        public static final int dev=0x7f050011;
+        public static final int exit=0x7f050009;
+        public static final int graphic=0x7f050012;
+        public static final int grezzana=0x7f05000d;
+        public static final int iervasi=0x7f050010;
+        public static final int main_title=0x7f050002;
+        public static final int noconnection=0x7f05000a;
+        public static final int noconnectioncalendar=0x7f050014;
+        public static final int nodescription=0x7f050015;
+        public static final int notavailable=0x7f050016;
+        public static final int otdev=0x7f050013;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
