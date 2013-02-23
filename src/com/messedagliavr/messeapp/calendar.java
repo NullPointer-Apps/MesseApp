42b01792ce4c185da0c5aafa66dcff3d75d6ccf8
Francesco <ambro.f@hotmail.com>
2013-02-23 14:41:51 +0100

    Fix Database(in teoria funziona)

--- src/com/messedagliavr/messeapp/calendar.java
+++ src/com/messedagliavr/messeapp/calendar.java
@@ -121,7 +121,7 @@ public class calendar extends ListActivity {
 		}
 		
 		public boolean checkForTables() {
-			Boolean hasTables=null;
+			Boolean hasTables=false;
 			Database databaseHelper = new Database(getBaseContext());
 			db = databaseHelper.getWritableDatabase();
 			Cursor cursor = db.rawQuery("SELECT * FROM calendar", null);
@@ -197,7 +197,7 @@ public class calendar extends ListActivity {
 						long newRowId = db.updateWithOnConflict("calendar", values, null,null,SQLiteDatabase.CONFLICT_REPLACE);	
 						}else {
 						db = databaseHelper.getWritableDatabase();
-						long newRowId = db.insert("calendar", null, values);
+						long newRowId = db.insertWithOnConflict("calendar", null, values, SQLiteDatabase.CONFLICT_REPLACE);
 						}
 
 					}
