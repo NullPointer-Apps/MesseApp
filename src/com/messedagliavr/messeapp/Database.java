ad256c15136a8a1d6bd3df4614517c4d7349f47e
Simone de Blasiis <simone.deblasiis@gmail.com>
2013-03-19 11:23:36 +0100

    Aggiunta tabella foto

--- src/com/messedagliavr/messeapp/Database.java
+++ src/com/messedagliavr/messeapp/Database.java
@@ -6,10 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class Database extends SQLiteOpenHelper {
 	public static final String NOME_DB = "messeapp.db";
-	public static final int VERSIONE_DB = 4;
+	public static final int VERSIONE_DB = 5;
 
 	private static final String CREATE_NEWS = "CREATE TABLE news (_id INTEGER PRIMARY KEY AUTOINCREMENT,title text not null,titleb text not null,description text not null);";
 	private static final String CREATE_CALENDAR = "CREATE TABLE calendar (_id INTEGER PRIMARY KEY AUTOINCREMENT,title text not null,titleb text not null,description text,ical text not null);";
+	private static final String CREATE_CLASS = "CREATE TABLE class (fname text);";
 	private static final String CREATE_UPDATE = "CREATE TABLE lstchk (newsdate text,calendardate text);";
 	private static final String POPULATE_UPDATE = "INSERT INTO lstchk VALUES ('1995-01-19 23:40:20','1995-01-19 23:40:20');";
 
@@ -26,6 +27,7 @@ public class Database extends SQLiteOpenHelper {
 		db.execSQL(CREATE_CALENDAR);
 		db.execSQL(CREATE_UPDATE);
 		db.execSQL(POPULATE_UPDATE);
+		db.execSQL(CREATE_CLASS);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
