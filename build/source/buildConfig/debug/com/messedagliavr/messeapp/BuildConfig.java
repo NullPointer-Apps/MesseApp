e20c747aedc0266208b70256fd497d97de3c5a13
Simone de Blasiis <simone.deblasiis@gmail.com>
2013-09-29 12:32:20 +0200

    Migrazione ad Android Studio

--- build/source/buildConfig/debug/com/messedagliavr/messeapp/BuildConfig.java
+++ build/source/buildConfig/debug/com/messedagliavr/messeapp/BuildConfig.java
@@ -2,5 +2,6 @@
 package com.messedagliavr.messeapp;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
 }
